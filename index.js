751e302c4cb4c25de23f4979cbcabaf09da291f2
joeCodes05 <nathancodes05@gmail.com>
2023-12-06 08:36:16 +0100

    code updates

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require('cookie-parser')
+
+// routes
+const authRoutes = require('./routes/auth/auth.routes');
 
 const app = express();
 app.use(cors("*"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Content-Type", "Application/json");
+  next();
+});
+
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
